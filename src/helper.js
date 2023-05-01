99395e8c48593e7f030d9d2b75c0d123fc77ff93
Nikhil Nawgiri <the@realest.ninja>
2023-05-01 14:27:13 +0200

    Refactor gatherResponse func

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,23 @@
 export const gatherResponse = async (response) =>  {
-  return await response.json();
-}
+  const { headers } = response;
+  const contentType = headers.get('content-type');
+
+  if (contentType.includes('application/json')) {
+    const body = await response.json();
+    return JSON.stringify(body);
+  } else if (contentType.includes('application/text')) {
+    const body = await response.text();
+    return body;
+  } else if (contentType.includes('text/html')) {
+    const body = await response.text();
+    return body;
+  } else {
+    const body = await response.text();
+    return body;
+  }
+};
+
+const buildKvApiUrl = ({ accountIdentifier, kvNamespace, key }) => `https://api.cloudflare.com/client/v4/accounts/${accountIdentifier}/storage/kv/namespaces/${kvNamespace}/values/${key}`;
 
 export const writeKv = async ({ accountIdentifier, kvNamespace, apiToken, key, value }) => {
   const kvWriteApiUrl = `https://api.cloudflare.com/client/v4/accounts/${accountIdentifier}/storage/kv/namespaces/${kvNamespace}/values/${key}`;
