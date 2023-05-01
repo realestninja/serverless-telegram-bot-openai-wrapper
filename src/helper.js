4824e98c18b982594cab94ac6b255b8f397abc50
Nikhil Nawgiri <the@realest.ninja>
2023-05-01 14:02:32 +0200

    Add writeKv function to helper

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,23 @@
 export const gatherResponse = async (response) =>  {
   return await response.json();
 }
+
+export const writeKv = async ({ accountIdentifier, kvNamespace, apiToken, key, value }) => {
+  const kvWriteApiUrl = `https://api.cloudflare.com/client/v4/accounts/${accountIdentifier}/storage/kv/namespaces/${kvNamespace}/values/${key}`;
+
+  const body = typeof(value) !== "string"
+    ? JSON.stringify(value)
+    : value;
+
+  const options = {
+    method: "PUT",
+    body,
+    headers: {
+      Authorization: `Bearer ${apiToken}`
+    },
+  };
+
+  const cfApiResponse= await fetch(kvWriteApiUrl, options);
+  // const responseData = await gatherResponse(cfApiResponse);
+  // console.log(JSON.stringify(responseData, null, 4));
+}
