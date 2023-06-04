345c9fe3568aa83cbb470feb7f686599d7b80f69
Nikhil Nawgiri <the@realest.ninja>
2023-06-04 10:30:16 +0200

    Refactor telegram helper

--- src/telegram.js
+++ src/telegram.js
@@ -1,9 +1,20 @@
-import { gatherResponse } from "./helper";
+import { gatherResponse } from "./cfUtilities";
+import { isEmpty } from "./helper";
 
-export const sendMessageToTelegramUser = async ({ token, chatId, text }) => {
-  const telegramSendToUserUrl = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`
-  const telegramResponse = await fetch(telegramSendToUserUrl);
-  // const gatheredContent = JSON.parse(await gatherResponse(telegramResponse));
-  // console.log(JSON.stringify(gatheredContent, null, 4));
+export const sendMessageToTelegramUser = async ({ token, chatId, text, replyMarkup = {} }) => {
+  const telegramSendToUserUrl = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}${!isEmpty(replyMarkup) ? "&replymarkup=" + JSON.stringify(replyMarkup) : ""}`;
+  // console.log("telegramSendToUserUrl:", telegramSendToUserUrl);
+
+  try {
+    const telegramResponse = await fetch(telegramSendToUserUrl);
+    const gatheredContent = JSON.parse(await gatherResponse(telegramResponse));
+    console.log("gatheredContent:", gatheredContent);
+    if (!gatheredContent.ok) {
+      throw new Error(`Error sending message to Telegram user. HTTP status: ${gatheredContent.status}`);
+    }
+    return gatheredContent;
+  } catch (error) {
+    console.error("Error sending message to Telegram user:", error);
+    throw error;
+  }
 };
-
