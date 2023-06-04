5111e929a129457d533ba593f179feba9e090d54
Nikhil Nawgiri <the@realest.ninja>
2023-06-04 09:33:40 +0200

    Disable logging of response going out to telegram

--- src/telegram.js
+++ src/telegram.js
@@ -3,7 +3,7 @@ import { gatherResponse } from "./helper";
 export const sendMessageToTelegramUser = async ({ token, chatId, text }) => {
   const telegramSendToUserUrl = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`
   const telegramResponse = await fetch(telegramSendToUserUrl);
-  const gatheredContent = JSON.parse(await gatherResponse(telegramResponse));
+  // const gatheredContent = JSON.parse(await gatherResponse(telegramResponse));
   // console.log(JSON.stringify(gatheredContent, null, 4));
 };
 
