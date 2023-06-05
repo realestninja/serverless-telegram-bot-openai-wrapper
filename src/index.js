e1d499126c6f34584f44013968bd924fd813ab7c
Nikhil Nawgiri <the@realest.ninja>
2023-06-05 17:26:23 +0200

    Refactor main logic

--- src/index.js
+++ src/index.js
@@ -1,29 +1,45 @@
-import { handleNewUser } from "./handleNewUser";
 import { readKv, writeKv } from "./cfUtilities";
-import { callOpenAiAPI } from "./openai";
-import { sendMessageToTelegramUser } from "./telegram";
+import { OWNER_ACTION_GRANT_PERMISSION, USER_ACTION_REQUEST_PERMISSION } from "./constants/callbackConstants";
+import { handleIncomingMessage } from "./messageHandler";
+import { activateNewUser, handlePermissionRequest } from "./handleNewUser";
 
 export default {
 	async fetch(request, env, ctx) {
     if (request.method === "POST") {
+      const {
+        BOT_TOKEN,
+        BOT_OWNER_ID,
+        OPEN_AI_API_KEY,
+        CF_ACCOUNT_IDENTIFIER,
+        CF_KV_NAMESPACE_IDENTIFIER,
+        CF_API_TOKEN,
+      } = env;
       const payload = await request.json()
+
+      // handle callbacks
+      if ("callback_query" in payload) {
+        const callbackAction = payload.callback_query.data;
+        console.log("callbackAction:", callbackAction);
+
+        if (callbackAction === USER_ACTION_REQUEST_PERMISSION) {
+          await handlePermissionRequest({ payload, BOT_TOKEN, BOT_OWNER_ID });
+        }
+
+        if (callbackAction.includes(OWNER_ACTION_GRANT_PERMISSION)) {
+          const userIdOfNewUser = callbackAction.split("DATA:")[1];
+          await activateNewUser({
+            userIdOfNewUser,
+            accountIdentifier: CF_ACCOUNT_IDENTIFIER,
+            kvNamespace: CF_KV_NAMESPACE_IDENTIFIER,
+            apiToken: CF_API_TOKEN,
+          });
+          return new Response("OK");
+        }
+      }
+
+      // handle messages
       if ("message" in payload) {
-        const {
-          BOT_TOKEN,
-          BOT_OWNER_ID,
-          OPEN_AI_API_KEY,
-          CF_ACCOUNT_IDENTIFIER,
-          CF_KV_NAMESPACE_IDENTIFIER,
-          CF_API_TOKEN,
-        } = env;
-        const { message } = payload;
-
-        const chatId = message.chat.id;
-
-        // ignore unknown people
-        if (chatId !== Number(BOT_OWNER_ID)) return null;
-        const openAiResponse = await callOpenAiAPI({ prompt: message.text, bearer: OPEN_AI_API_KEY });
-        await sendMessageToTelegramUser({ token: BOT_TOKEN, chatId, text: openAiResponse });
+        await handleIncomingMessage({ payload, BOT_TOKEN, BOT_OWNER_ID, OPEN_AI_API_KEY })
       }
     }
 
